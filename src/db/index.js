f8d79f996ed48e60e9cd4fd7db3dce6aca7f70ee
Felicia Okta <feliciaokta@outlook.com>
2021-06-16 20:37:17 -0700

    updated package.json for cypress

--- src/db/index.js
+++ src/db/index.js
@@ -2,9 +2,9 @@ const pg = require("pg");
 
 const client = new pg.Client({
   connectionString: process.env.DATABASE_URL || "",
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  // ssl: {
+  //   rejectUnauthorized: false,
+  // },
 });
 
 client
